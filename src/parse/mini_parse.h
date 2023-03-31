c48aadedd676253a92d387b3361bed0056c1c921
christie26 <yoonseo9800@gmail.com>
2023-03-31 12:10:17 +0900

    parse - tokenizer works except env, quote, parenthesis

---
+++ src/parse/mini_parse.h
@@ -0,0 +1,21 @@
+
+#ifndef MINI_PARSE_H
+
+# define MINI_PARSE_H
+
+#include <readline/readline.h>
+#include <readline/history.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+
+# include "../../libft/src/libft.h"
+
+int ft_is_space(char c);
+int ft_is_word(char c);
+
+
+
+
+#endif
