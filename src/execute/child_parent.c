21f4fa788ce0a896f5edeb765bba110552952810
christie26 <yoonseo9800@gmail.com>
2023-04-23 13:25:16 +0900

    check cmd valid before make pipe -> prevent error case

--- src/execute/child_parent.c
+++ src/execute/child_parent.c
@@ -4,19 +4,11 @@ void	ft_execute(char **options, t_data *data)
 {
 	char	*cmd;
 
-	cmd = ft_strdup(options[0]);
-	ft_err_msg_exit(!cmd, MALLOC_ERROR, __FILE__, __LINE__);
+	cmd = options[0];
 	if (is_builtin(cmd))
 		exit(ft_builtin(options, data));
 	else
 	{
-		cmd = check_access(cmd, data->path);
-		if (!cmd)
-		{
-			ft_err_msg(1, CMD_ERROR, __FILE__, __LINE__);
-			short_exit_status = 127;
-			return ;
-		}
 		if (execve(cmd, options, get_env(data->my_env)) == -1)
 			ft_err_sys_exit(1, __FILE__, __LINE__);
 	}
