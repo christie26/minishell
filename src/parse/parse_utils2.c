8d31567a0ba782992f60899d1acb78bb9d0cb88d
YongHyun-Yi <dldydgus1995@naver.com>
2023-04-25 13:03:11 +0900

    함수 리뉴얼 중...

--- src/parse/parse_utils2.c
+++ src/parse/parse_utils2.c
@@ -7,85 +7,120 @@ void print_tokens(void *content)
 	ft_putendl_fd(content, 1);
 }
 
-int		get_word_token(t_list **tokens, char **str)
+char *get_operator(char **input)
 {
-	/*
+	char	*substr_offset;
+	char	*token_content;
+	
+	substr_offset = *input;
+	++*input;
+	if (**input && (*(*input - 1) == **input && ft_strchr("<>", **input)))
+		++*input;
+	token_content = ft_substr(substr_offset, 0, *input - substr_offset);
+	if (token_content == NULL)
+		exit(EXIT_FAILURE);
+	return (token_content);
+}
 
-		호출조건
-		- 메타캐릭터가 아닐것
-		- 널문자가 아닐것
-
-	*/
-
+char *get_word_with_quote(char **input)
+{
 	char *substr_offset;
 	char open_quote;
-	char *token_str;
-	t_list *new_token;
+	char *token_content;
 
-	substr_offset = *str;
+	substr_offset = *input;
 	open_quote = '\0';
-	while (**str)
+	while (**input)
 	{
 		if (open_quote == '\0')
 		{
-			if (is_metacharacter(**str))
+			if (is_metacharacter(**input))
 				break ;
-			else if (is_quote(**str))
-				open_quote = **str;
+			else if (is_quote(**input))
+				open_quote = **input;
 		}
-		else if (open_quote && open_quote == **str)
+		else if (open_quote && open_quote == **input)
 			open_quote = '\0';
-		++*str;
+		++*input;
 	}
-	token_str = ft_substr(substr_offset, 0, *str - substr_offset);
-	new_token = ft_lstnew(token_str);
-	ft_lstadd_back(tokens, new_token);
-	return (1);
+	token_content = ft_substr(substr_offset, 0, *input - substr_offset);
+	if (token_content == NULL)
+		exit(EXIT_FAILURE);
+	return (token_content);
 }
 
-int get_operator_token(t_list **tokens, char **str)
+char *get_word_without_quote(char **input)
 {
-	/*
+	char *str;
+	char open_quote;
+	char *token_content;
 
-		호출조건
-		- 메타캐릭터일것
-		- 따옴표가 아닐것
-		- 널문자가 아닐것
+	size_t len;
 
-	*/
-
-	char	*substr_offset;
-	char	*token_str;
-	t_list	*new_token;
-	
-	substr_offset = *str;
-	++*str;
-	if (**str && (*(*str - 1) == **str && ft_strchr("<>", **str)))
-		++*str;
-	token_str = ft_substr(substr_offset, 0, *str - substr_offset);
-	new_token = ft_lstnew(token_str);
-	ft_lstadd_back(tokens, new_token);
-	return (1);
-}
-
-/*
-
-	따옴표가 열려있으면 짝맞추기를 안해도됨?
-	말그대로 리터럴이 되어버리고
-	따옴표 안에서는 특별한 의미를 잃어버리기 때문 -> 확장포함
-	그럼 확장의 주도권을 따옴표 제거가 가져야 하나?
-
-*/
-
-void	create_tokens(t_list **tokens, char *str)
-{
+	str = *input;
+	open_quote = '\0';
+	len = 0;
 	while (*str)
 	{
-		if (!is_metacharacter(*str))
-			get_word_token(tokens, &str);
-		else if (is_operator_char(*str))
-			get_operator_token(tokens, &str);
-		else
-			str++;
+		if ((open_quote == '\0' && ft_isalnum(*str)) || open_quote) // 순수문자거나 인용문이거나
+			len++;
+		else if (is_quote(*str)) // 따옴표 처리
+		{
+			if (open_quote == '\0')
+				open_quote = *str;
+			else if (open_quote == *str)
+				open_quote = '\0';
+		}
+		else // 결과물에 들어가는 문자가 아닌경우
+			break ;
+		++str;
+	}
+	
+	token_content = ft_calloc(1, len);
+	if (token_content == NULL)
+		exit(EXIT_FAILURE);
+	
+	open_quote = '\0';
+	while (**input)
+	{
+		if ((open_quote == '\0' && ft_isalnum(**input)) || open_quote) // 순수문자거나 인용문이거나
+			*token_content++ = **input;
+		else if (is_quote(**input)) // 따옴표 처리
+		{
+			if (open_quote == '\0')
+				open_quote = **input;
+			else if (open_quote == **input)
+				open_quote = '\0';
+		}
+		else // 결과물에 들어가는 문자가 아닌경우
+			break ;
+		++*input;
+	}
+	
+	return (token_content);
+}
+
+void	create_tokens(t_list **tokens, char *input)
+{
+	t_list *new_token;
+
+	while (*input)
+	{
+		while (is_blank(*input))
+			input++;
+		if (!is_metacharacter(*input))
+		{
+			new_token = ft_lstnew(get_word_with_quote(*input));
+			if (new_token == NULL)
+				exit(EXIT_FAILURE);
+			ft_lstadd_back(tokens, new_token);
+		}
+		else if (is_operator_char(*input))
+		{
+			new_token = ft_lstnew(get_operator(*input));
+			if (new_token == NULL)
+				exit(EXIT_FAILURE);
+			ft_lstadd_back(tokens, new_token);
+		}
 	}
 }
